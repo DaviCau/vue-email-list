55835e3b9f9ee4b5584164e474bc3539a33a9efc
DaviCau <davide.cau.rn@gmail.com>
2021-05-27 17:00:50 +0200

    stampati 10 indirizzi email casuali

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var app = new Vue ({
+    el: "#root",
+    data: {
+        emails: []
+    },
+    created: function() {
+        for (var i = 0; i < 10; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((result) => {
+                this.emails.push(result.data.response);
+            });
+        }
+    }
+});
